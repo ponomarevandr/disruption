ab417c3592d4de7d5e99ffe87ac51ac9213eb6a1
Andrey Ponomarev <ponomarev.andr2002@gmail.com>
2023-10-24 01:08:26 +0300

    An example added

--- one_dim_problem/one_dim.cpp
+++ one_dim_problem/one_dim.cpp
@@ -4,19 +4,19 @@
 
 
 const double eps_0 = 1e-3;
-const double delta = 1e-5;
+const double delta = 1e-6;
 const double l = 1.0;
-const double Gamma = 1.0;
+const double Gamma = 0.01;
 const double m = 1.0;
 const double Phi_coefficient = 1.0;
 
 const double duration = 1.0;
 const double width = 10;
 
-constexpr size_t X_SIZE = 100;
-constexpr size_t T_SIZE = 100;
-constexpr size_t X_SKIP = 10;
-constexpr size_t T_SKIP = 10;
+constexpr size_t X_SIZE = 1000;
+constexpr size_t T_SIZE = 100000;
+constexpr size_t X_SKIP = 1;
+constexpr size_t T_SKIP = 1000;
 
 const char* output_filename = "phi_values.csv";
 const size_t precision = 4;
@@ -29,7 +29,7 @@ void initialize() {
 	for (size_t i = 0; i < X_SIZE; ++i) {
 		phi[i] = 1.0;
 	}
-	phi[X_SIZE / 2] = 0.9;
+	phi[X_SIZE / 2] = 0.5;
 }
 
 double f(double phi) {
@@ -58,6 +58,7 @@ void iteration() {
 	for (size_t i = 1; i < X_SIZE - 1; ++i) {
 		phi[i] = phi_next[i];
 		phi[i] = std::min(phi[i], 1.0);
+		phi[i] = std::max(phi[i], 0.0);
 	}
 }
 
