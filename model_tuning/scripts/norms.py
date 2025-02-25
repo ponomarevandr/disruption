98190ed9e4a1b6d54b58c382cb37df2401ddf86c
Andrey Ponomarev <ponomarev.andr2002@gmail.com>
2025-02-25 12:12:00 +0300

    A t-shift metric script added; the models in the case of channel birth measured

--- model_tuning/scripts/norms.py
+++ model_tuning/scripts/norms.py
@@ -1,64 +1,69 @@
 import numpy as np
 
 
+def common_t_indices(dt_level_first, dt_level_second):
+	dt_levels = [dt_level_first, dt_level_second]
+	t_indices = ([], [])
+	index = [0, 0]
+	while index[0] < len(dt_levels[0]) and index[1] < len(dt_levels[1]):
+		i_rough = int(dt_levels[0][index[0]] < dt_levels[1][index[1]])
+		i_precise = 1 - i_rough
+		scale_rough = 1 << dt_levels[i_rough][index[i_rough]]
+		index[i_rough] += 1
+		scale_precise = 0
+		while scale_precise < scale_rough:
+			scale_precise += 1 << dt_levels[i_precise][index[i_precise]]
+			index[i_precise] += 1
+		t_indices[i_rough].append(index[i_rough] - 1)
+		t_indices[i_precise].append(index[i_precise] - 1)
+	return t_indices
+
 def get_skips(grid_first, grid_second):
 	grid = min(grid_first, grid_second)
 	if grid_first % grid != 0 or grid_second % grid != 0:
 		return -1, -1, -1
 	return grid, grid_first // grid, grid_second // grid
 
-def norm_uniform_1d(values_first, values_second):
-	_, skip_first, skip_second = \
-		get_skips(len(values_first) - 1, len(values_second) - 1)
-	if skip_first == -1:
-		return -1
-	values_numpy_first = np.array(values_first[::skip_first])
-	values_numpy_second = np.array(values_second[::skip_second])
-	return np.max(np.abs(values_numpy_first - values_numpy_second))
-
-def norm_uniform_2d(values_first, values_second):
+def phi_difference(model_first, model_second):
+	t_indices_first, t_indices_second = \
+		common_t_indices(model_first._dt_level, model_second._dt_level)
 	_, x_skip_first, x_skip_second = \
-		get_skips(values_first.shape[1] - 1, values_second.shape[1] - 1)
-	_, t_skip_first, t_skip_second = \
-		get_skips(values_first.shape[0] - 1, values_second.shape[0] - 1)
-	if x_skip_first == -1 or t_skip_first == -1:
-		return -1
-	values_numpy_first = values_first.iloc[::t_skip_first, ::x_skip_first].to_numpy()
-	values_numpy_second = values_second.iloc[::t_skip_second, ::x_skip_second].to_numpy()
-	return np.max(np.abs(values_numpy_first - values_numpy_second))
+		get_skips(model_first.df_phi.shape[1] - 1, model_second.df_phi.shape[1] - 1)
+	values_numpy_first = model_first.df_phi.iloc[t_indices_first, ::x_skip_first].to_numpy()
+	values_numpy_second = model_second.df_phi.iloc[t_indices_second, ::x_skip_second].to_numpy()
+	return values_numpy_first - values_numpy_second, model_first.ts[t_indices_first]
 
-def norm_uniform(model_first, model_second):
-	return norm_uniform_2d(model_first.df_phi, model_second.df_phi)
+def norm_uniform(values):
+	return np.abs(values).max()
 
-def norm_uniform_final(model_first, model_second):
-	return norm_uniform_1d(model_first.df_phi.iloc[-1], model_second.df_phi.iloc[-1])
+def optimal_norm_between(value, first, second):
+	index_max = np.abs(second - first).argmax()
+	coef = (value[index_max] - first[index_max]) / (second[index_max] - first[index_max])
+	return norm_uniform(first + coef * (second - first) - value), coef
 
-def norm_L2_1d(values_first, values_second, width):
-	grid, skip_first, skip_second = \
-		get_skips(len(values_first) - 1, len(values_second) - 1)
-	if skip_first == -1:
-		return -1
-	values_numpy_first = np.array(values_first[::skip_first])
-	values_numpy_second = np.array(values_second[::skip_second])
-	coefficient = width / (grid + 1)
-	return np.sqrt(np.sum((values_numpy_first - values_numpy_second) ** 2) * coefficient)
-
-def norm_L2_2d(values_first, values_second, width, duration):
-	x_grid, x_skip_first, x_skip_second = \
-		get_skips(values_first.shape[1] - 1, values_second.shape[1] - 1)
-	t_grid, t_skip_first, t_skip_second = \
-		get_skips(values_first.shape[0] - 1, values_second.shape[0] - 1)
-	if x_skip_first == -1 or t_skip_first == -1:
-		return -1
-	values_numpy_first = values_first.iloc[::t_skip_first, ::x_skip_first].to_numpy()
-	values_numpy_second = values_second.iloc[::t_skip_second, ::x_skip_second].to_numpy()
-	coefficient = width * duration / (x_grid + 1) / (t_grid + 1)
-	return np.sqrt(np.sum((values_numpy_first - values_numpy_second) ** 2) * coefficient)
-
-def norm_L2(model_first, model_second):
-	return norm_L2_2d(model_first.df_phi, model_second.df_phi,
-		model_first.params['width'], model_first.params['duration'])
-
-def norm_L2_final(model_first, model_second):
-	return norm_L2_1d(model_first.df_phi.iloc[-1], model_second.df_phi.iloc[-1],
-		model_first.params['width'])
+def metric_shifted(values_first, ts_first, values_second, ts_second):
+	metric = np.zeros_like(ts_first)
+	t_shift = np.zeros_like(ts_first)
+	index_second = 0
+	for index_first in range(len(ts_first)):
+		metric_current, coef_current = optimal_norm_between(
+			values_first.iloc[index_first],
+			values_second.iloc[index_second],
+			values_second.iloc[index_second + 1]
+		)
+		while index_second + 2 < len(ts_second) and coef_current > -0.5:
+			metric_next, coef_next = optimal_norm_between(
+				values_first.iloc[index_first],
+				values_second.iloc[index_second + 1],
+				values_second.iloc[index_second + 2]
+			)
+			if metric_next >= metric_current and coef_current < 1.5:
+				break
+			metric_current = metric_next
+			coef_current = coef_next
+			index_second += 1
+		metric[index_first] = metric_current
+		t_shift[index_first] = \
+			ts_second[index_second] - ts_first[index_first] + \
+			coef_current * (ts_second[index_second + 1] - ts_second[index_second])
+	return metric, t_shift
---
+++ model_tuning/scripts/norms_old.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+
+def get_skips(grid_first, grid_second):
+	grid = min(grid_first, grid_second)
+	if grid_first % grid != 0 or grid_second % grid != 0:
+		return -1, -1, -1
+	return grid, grid_first // grid, grid_second // grid
+
+def norm_uniform_1d(values_first, values_second):
+	_, skip_first, skip_second = \
+		get_skips(len(values_first) - 1, len(values_second) - 1)
+	if skip_first == -1:
+		return -1
+	values_numpy_first = np.array(values_first[::skip_first])
+	values_numpy_second = np.array(values_second[::skip_second])
+	return np.max(np.abs(values_numpy_first - values_numpy_second))
+
+def norm_uniform_2d(values_first, values_second):
+	_, x_skip_first, x_skip_second = \
+		get_skips(values_first.shape[1] - 1, values_second.shape[1] - 1)
+	_, t_skip_first, t_skip_second = \
+		get_skips(values_first.shape[0] - 1, values_second.shape[0] - 1)
+	if x_skip_first == -1 or t_skip_first == -1:
+		return -1
+	values_numpy_first = values_first.iloc[::t_skip_first, ::x_skip_first].to_numpy()
+	values_numpy_second = values_second.iloc[::t_skip_second, ::x_skip_second].to_numpy()
+	return np.max(np.abs(values_numpy_first - values_numpy_second))
+
+def norm_uniform(model_first, model_second):
+	return norm_uniform_2d(model_first.df_phi, model_second.df_phi)
+
+def norm_uniform_final(model_first, model_second):
+	return norm_uniform_1d(model_first.df_phi.iloc[-1], model_second.df_phi.iloc[-1])
+
+def norm_L2_1d(values_first, values_second, width):
+	grid, skip_first, skip_second = \
+		get_skips(len(values_first) - 1, len(values_second) - 1)
+	if skip_first == -1:
+		return -1
+	values_numpy_first = np.array(values_first[::skip_first])
+	values_numpy_second = np.array(values_second[::skip_second])
+	coefficient = width / (grid + 1)
+	return np.sqrt(np.sum((values_numpy_first - values_numpy_second) ** 2) * coefficient)
+
+def norm_L2_2d(values_first, values_second, width, duration):
+	x_grid, x_skip_first, x_skip_second = \
+		get_skips(values_first.shape[1] - 1, values_second.shape[1] - 1)
+	t_grid, t_skip_first, t_skip_second = \
+		get_skips(values_first.shape[0] - 1, values_second.shape[0] - 1)
+	if x_skip_first == -1 or t_skip_first == -1:
+		return -1
+	values_numpy_first = values_first.iloc[::t_skip_first, ::x_skip_first].to_numpy()
+	values_numpy_second = values_second.iloc[::t_skip_second, ::x_skip_second].to_numpy()
+	coefficient = width * duration / (x_grid + 1) / (t_grid + 1)
+	return np.sqrt(np.sum((values_numpy_first - values_numpy_second) ** 2) * coefficient)
+
+def norm_L2(model_first, model_second):
+	return norm_L2_2d(model_first.df_phi, model_second.df_phi,
+		model_first.params['width'], model_first.params['duration'])
+
+def norm_L2_final(model_first, model_second):
+	return norm_L2_1d(model_first.df_phi.iloc[-1], model_second.df_phi.iloc[-1],
+		model_first.params['width'])
